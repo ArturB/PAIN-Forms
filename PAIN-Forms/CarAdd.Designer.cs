4c072f4cba6888098499540cb5915a6558a82d89
ArturB <artur.brodzki@gmail.com>
2017-04-02 11:57:28 +0200

    non important

--- PAIN-Forms/CarAdd.Designer.cs
+++ PAIN-Forms/CarAdd.Designer.cs
@@ -55,7 +55,8 @@
             this.tableLayoutPanel1.Controls.Add(this.PredkoscTextBox, 1, 2);
             this.tableLayoutPanel1.Controls.Add(this.button1, 1, 4);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 20);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 16);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 5;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
@@ -63,16 +64,17 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(477, 379);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(358, 308);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Location = new System.Drawing.Point(2, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(113, 56);
+            this.label1.Size = new System.Drawing.Size(85, 46);
             this.label1.TabIndex = 0;
             this.label1.Text = "Marka";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -81,9 +83,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label2.Location = new System.Drawing.Point(3, 56);
+            this.label2.Location = new System.Drawing.Point(2, 46);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(113, 56);
+            this.label2.Size = new System.Drawing.Size(85, 46);
             this.label2.TabIndex = 1;
             this.label2.Text = "Rok prod.";
             this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -92,9 +95,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label3.Location = new System.Drawing.Point(3, 112);
+            this.label3.Location = new System.Drawing.Point(2, 92);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(113, 56);
+            this.label3.Size = new System.Drawing.Size(85, 46);
             this.label3.TabIndex = 2;
             this.label3.Text = "Prędkość maks.";
             this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -103,37 +107,40 @@
             // 
             this.tableLayoutPanel1.SetColumnSpan(this.markaTextBox, 2);
             this.markaTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.markaTextBox.Location = new System.Drawing.Point(122, 3);
+            this.markaTextBox.Location = new System.Drawing.Point(91, 2);
+            this.markaTextBox.Margin = new System.Windows.Forms.Padding(2);
             this.markaTextBox.Name = "markaTextBox";
-            this.markaTextBox.Size = new System.Drawing.Size(232, 22);
+            this.markaTextBox.Size = new System.Drawing.Size(174, 20);
             this.markaTextBox.TabIndex = 4;
             // 
             // rokTextBox
             // 
             this.tableLayoutPanel1.SetColumnSpan(this.rokTextBox, 2);
             this.rokTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.rokTextBox.Location = new System.Drawing.Point(122, 59);
+            this.rokTextBox.Location = new System.Drawing.Point(91, 48);
+            this.rokTextBox.Margin = new System.Windows.Forms.Padding(2);
             this.rokTextBox.Name = "rokTextBox";
-            this.rokTextBox.Size = new System.Drawing.Size(232, 22);
+            this.rokTextBox.Size = new System.Drawing.Size(174, 20);
             this.rokTextBox.TabIndex = 5;
             // 
             // PredkoscTextBox
             // 
             this.tableLayoutPanel1.SetColumnSpan(this.PredkoscTextBox, 2);
             this.PredkoscTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.PredkoscTextBox.Location = new System.Drawing.Point(122, 115);
+            this.PredkoscTextBox.Location = new System.Drawing.Point(91, 94);
+            this.PredkoscTextBox.Margin = new System.Windows.Forms.Padding(2);
             this.PredkoscTextBox.Name = "PredkoscTextBox";
-            this.PredkoscTextBox.Size = new System.Drawing.Size(232, 22);
+            this.PredkoscTextBox.Size = new System.Drawing.Size(174, 20);
             this.PredkoscTextBox.TabIndex = 6;
             // 
             // button1
             // 
             this.tableLayoutPanel1.SetColumnSpan(this.button1, 2);
             this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button1.Location = new System.Drawing.Point(139, 329);
-            this.button1.Margin = new System.Windows.Forms.Padding(20, 10, 20, 10);
+            this.button1.Location = new System.Drawing.Point(104, 269);
+            this.button1.Margin = new System.Windows.Forms.Padding(15, 8, 15, 8);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(198, 40);
+            this.button1.Size = new System.Drawing.Size(148, 31);
             this.button1.TabIndex = 7;
             this.button1.Text = "Zapisz";
             this.button1.UseVisualStyleBackColor = true;
@@ -141,13 +148,14 @@
             // 
             // CarAdd
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(477, 399);
+            this.ClientSize = new System.Drawing.Size(358, 324);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "CarAdd";
-            this.Padding = new System.Windows.Forms.Padding(0, 20, 0, 0);
+            this.Padding = new System.Windows.Forms.Padding(0, 16, 0, 0);
             this.Text = "Add a car";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.CarEdit_FormClosing);
             this.Load += new System.EventHandler(this.CarEdit_Load);
